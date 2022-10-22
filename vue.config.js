96d0ddd36e7c4a6ad1115731666d4e048f3b7e8a
jmleeh4 <jmleeh4@naver.com>
2022-10-22 12:39:37 +0900

    testCommit1

--- vue.config.js
+++ vue.config.js
@@ -6,6 +6,7 @@ module.exports = defineConfig({
     proxy: {
       '/api': {
         // '/api' 로 들어오면 포트 8081(스프링 서버)로 보낸다
+        //20221022
         target: 'http://localhost:8081',
         changeOrigin: true // cross origin 허용
       }
