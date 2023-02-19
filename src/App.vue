30421a234c21c22e89c24da52d9f9fb7233356c3
jhleeh4 <jmleeh4@naver.com>
2023-02-20 00:54:11 +0900

    20230220

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,82 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <v-app id="app">
+    <v-main>
+      <v-container
+          style="position: relative; top: 20%; margin-left: 20%"
+          class="text-xs-center"
+      >
+        <v-layout row wrap class="text-xs-center">
+          <v-col>
+            <v-card flat class="mx-auto" max-width="800">
+              <v-row style="margin-top: 60px">
+                <v-col>
+                  <v-form style="width: 400px; height: 300px">
+                    <div class="mx-3">
+                      <v-icon color="black" size="30px">person</v-icon>
+                      userId
+                      <div class="mx-1">
+                        <v-text-field
+                            placeholder="userId"
+                            v-model="userId"
+                            required
+                        ></v-text-field>
+                      </div>
+                    </div>
+                    <div class="mx-3">
+                      <v-icon color="black" size="30px">lock</v-icon>
+                      userPassword
+                      <div class="mx-1">
+                        <v-text-field
+                            placeholder="userPassword"
+                            type="password"
+                            v-model="password"
+                            required
+                        ></v-text-field>
+                      </div>
+                    </div>
+
+                    <v-card-actions>
+                      <v-btn
+                          color="#2c4f91"
+                          dark
+                          large
+                          block
+                          @click="loginSubmit"
+                      >Login</v-btn
+                      >
+                    </v-card-actions>
+                  </v-form>
+                </v-col>
+              </v-row>
+            </v-card>
+          </v-col>
+        </v-layout>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
 
 export default {
+
   name: 'App',
   components: {
-    HelloWorld
+
+  },
+  data(){
+    return{
+      userId : "",
+      password : "",
+    }
+  },
+  methods : {
+    loginSubmit() {
+      alert('test');
+    },
+
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,22 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+    icons: {
+        defaultSet: 'mdi',
+        aliases,
+        sets: {
+            mdi,
+        }
+    },
+})
+
+createApp(App).use(vuetify).mount('#app')
