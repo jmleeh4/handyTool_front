1b2422acfc8f26bc50219805e4768e814f6bc888
jhleeh4 <jmleeh4@naver.com>
2023-02-24 00:19:16 +0900

    20230223 로그인, 회원가입 id,password 유효성검사 구현중

--- src/components/account/Registration.vue
+++ src/components/account/Registration.vue
@@ -1,14 +1,14 @@
 <template>
   <v-main>
     <v-container
-        style="position: relative; top: 20%; margin: auto"
-        class="text-xs-center"
+        style="position: relative; margin: auto;"
+        class="text-xs-center h-100"
     >
-      <v-layout row wrap class="text-xs-center">
-        <v-col style="height: 700px">
-          <v-card  class="mx-auto" max-width="800">
-            <v-row style="margin-top: 60px">
-              <v-col style="margin: auto; height: 600px">
+      <v-layout row wrap class="text-xs-center h-100">
+        <v-col>
+          <v-card  class="mx-auto h-100" max-width="800">
+            <v-row style="margin-top: 60px;">
+              <v-col style="margin: auto;">
                 <v-form style="width: 400px; height: 500px; margin: auto; margin-top: 20px" >
                   <div class="mx-3">
                     <div class="mx-3 font-weight-black text-h4" style="text-align: center; margin-bottom: 70px; color: cornflowerblue">
@@ -17,18 +17,23 @@
                     </div>
                     <div class="mx-1">
                       <v-text-field
-                          placeholder="userId"
-                          v-model="userId"
-                          required
+                          v-model   ="userId"
+                          :counter  ="15"
+                          :rules    ="idRule"
+                          label     ="Name"
+                          maxLength = 15
                       ></v-text-field>
                     </div>
                   </div>
                   <div class="mx-3">
                     <div class="mx-1">
                       <v-text-field
-                          placeholder="userPassword"
                           type="password"
                           v-model="password"
+                          :rules="pwRule"
+                          :counter  = 20
+                          maxLength = 20
+                          label="Password"
                           required
                       ></v-text-field>
                     </div>
@@ -63,8 +68,38 @@
 
 <script>
 export default {
-  name: "Registration"
+  name: "Registration",
+  data(){
+    return {
+      idRule : [
+          value => !!value || '아이디를 입력해주세요.',
+          value => {
+            const trimValue = value.replace(/(\s*)/g, '');
+            // const pattern = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/
+            return trimValue.length > 7 || '아이디는 7자리 이상입니다';
+          }
+      ],
+      pwRule : [
+        value => !!value || '비밀번호를 입력하세요',
+        value => {
+          const trimValue = value.replace(/(\s*)/g, '');
+          return trimValue.length > 10 || '비밀번호는 10자리 이상입니다';
+        }
+      ],
 
+      userId    : "",
+      password  : "",
+
+    }
+  },
+  created() {
+
+  },
+  methods : {
+    idRule(){
+      this.name.value = "";
+    }
+  }
 }
 </script>
 
