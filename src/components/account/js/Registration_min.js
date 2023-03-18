0ce9347eff7f006dd458fd8f9b0aa28280fc7852
jhleeh4 <jmleeh4@naver.com>
2023-03-18 23:46:12 +0900

    20230309 commit

--- src/components/account/js/Registration_min.js
+++ src/components/account/js/Registration_min.js
@@ -41,6 +41,7 @@ export default {
                     }
                 }
             ],
+            //이메일 룰
             emailRule : [
                 value => {
                     const regExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i
