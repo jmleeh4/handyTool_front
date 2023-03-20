fa8ef1eccc557b75d34749b252e2ba2a9c391d56
jhleeh4 <jmleeh4@naver.com>
2023-03-20 23:05:51 +0900

    20230320 push

--- src/components/account/js/Registration_min.js
+++ src/components/account/js/Registration_min.js
@@ -47,6 +47,7 @@ export default {
                 value => {
                     const regExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i
                     return value.match(regExp) || "잘못된 이메일 형식입니다";
+
                 }
             ],
 
